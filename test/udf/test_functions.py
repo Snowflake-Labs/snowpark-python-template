20218262afe6b7633e2a40b7f0722ee2b9b13068
Jason Freeberg <jason.freeberg@snowflake.com>
2023-05-12 18:25:46 -0700

    In progress: exploring testing when using @udf

--- test/udf/test_functions.py
+++ test/udf/test_functions.py
@@ -1,7 +1,15 @@
-from src.udf.functions import combine
 
+import pytest
+from snowflake.snowpark.session import Session
+from src.util.local import get_env_var_config
 
-def test_combine():
+@pytest.fixture
+def session():
+    return Session.builder.configs(get_env_var_config()).create()
+
+def test_combine(session):
+    from src.udf.functions import combine
     expected = "hello world"
     actual = combine("hello ", "world")
+    import pdb; pdb.set_trace()
     assert expected == actual
