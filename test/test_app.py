71d8c19302b1630d75da031c8f6c6abd1eca9614
Jason Freeberg <jason.freeberg@snowflake.com>
2023-08-02 15:24:24 -0700

    Update test_app.py

--- test/test_app.py
+++ test/test_app.py
@@ -7,6 +7,7 @@ from src.app import run
 
 
 def test_app_dim(session: Session):
+    session.add_import("src/functions.py", 'src.functions')
     expected = session.create_dataframe(
         [["Welcome to Snowflake!"], ["Learn more: https://www.snowflake.com/snowpark/"]],
         ["hello_world"])
