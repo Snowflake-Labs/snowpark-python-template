726419ae465ed7662e71469835cdeaef8c31070f
Jason Freeberg <jason.freeberg@snowflake.com>
2023-08-04 13:07:38 -0700

    Update test_app.py

--- test/test_app.py
+++ test/test_app.py
@@ -4,10 +4,10 @@ Tests for the procedure module.
 
 from snowflake.snowpark.session import Session
 from src.app import run
-
+from src import functions
 
 def test_app_dim(session: Session):
-    session.add_import("src/functions.py", 'src.functions')
+    session.add_import(functions.__file__, 'src.functions')
     expected = session.create_dataframe(
         [["Welcome to Snowflake!"], ["Learn more: https://www.snowflake.com/snowpark/"]],
         ["hello_world"])
